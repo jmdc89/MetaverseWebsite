344e21b3b6f3dc9396361cb597796e298cf322db
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-01-30 15:46:11 +0300

    Update World.jsx
    imports

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+
 const World = () => (
   <section>
     World section
