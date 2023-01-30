b44afad7c6de9b8159f8378329b8e73d0b3a9318
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-01-30 15:57:41 +0300

    Update World.jsx
    imports2

--- sections/World.jsx
+++ sections/World.jsx
@@ -3,14 +3,5 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { newFeatures } from '../constants';
-import { NewFeatures, TitleText, TypingText } from '../components';
-import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
-
-const World = () => (
-  <section>
-    World section
-  </section>
-);
-
-export default World;
+import { TitleText, TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
