4d106410910f3d03674b1950682b1afe43f86414
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-01-30 16:17:56 +0300

    Update Footer.jsx
    import

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
 const Footer = () => (
   <footer>
     footer
